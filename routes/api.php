cd304d3223c193bdf7bd94d09bf82d357f116baa
BrwaAta <brwa.ata.hamalaw@gmail.com>
2017-08-23 16:27:35 -0700

    Create all routes

--- routes/api.php
+++ routes/api.php
@@ -13,3 +13,20 @@ use Illuminate\Http\Request;
 |
 */
 
+/*  buyer Route  */
+Route::resource('buyers' , 'Buyer\BuyerController' , [ 'only'  => [ 'index' ,  'show']  ]);
+
+/*  seller Route  */
+Route::resource('seller' , 'Seller\SellerController' , [ 'only'  => [ 'index' ,  'show']  ]);
+
+/*  category Route  */
+Route::resource('category' , 'Category\CategoryController' , [ 'except'  => [ 'create' ,  'edit']  ]);
+
+/*  user Route  */
+Route::resource('user' , 'User\UserController' , [ 'except'  => [ 'create' ,  'edit']  ]);
+
+/*  transaction Route  */
+Route::resource('transaction' , 'Transaction\TransactionController' , [ 'only'  => [ 'index' ,  'show']  ]);
+
+/*  product Route  */
+Route::resource('product' , 'Product\ProductController' , [ 'only'  => [ 'index' ,  'show']  ]);
