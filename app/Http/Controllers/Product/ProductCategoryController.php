fb75ba9715a877a43320f0f7162c5159db965157
BrwaAta <brwa.ata.hamalaw@gmail.com>
2017-08-27 00:31:51 -0700

    Insert a new category with three defferent function

--- app/Http/Controllers/Product/ProductCategoryController.php
+++ app/Http/Controllers/Product/ProductCategoryController.php
@@ -25,59 +25,33 @@ class ProductCategoryController extends ApiController
 
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \Illuminate\Http\Request $request
+     * @param $product_id
+     * @param $category_id
      * @return \Illuminate\Http\Response
+     * @internal param int $id
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, $product_id , $category_id)
     {
-        //
+        $product  = Product::findOrFail($product_id);
+        $category  = Product::findOrFail($category_id);
+
+        /* BO KAR KRDN LASAR MANY to MANY RELATION CHAND FINCTIONAKE HAYA
+            1 / attach     ,   2 / sync    , 3 / syncWithoutDetaching
+            1/ har jare ka run abe danayaky  taza drwst aka w datakan dwbara abnawa
+            2/ kate run abe danayaky taza drwst akaw away peshtr ka habwa asretawa wata datay dwbara leraya bwny nyia
+            3/ amayan awaya ka AMANAWE chwnka danayaky taza drwst akaw hych karygayiaky lasar awany tr nyia
+        */
+
+        //$product->categories()->attach([$category->id]);
+        $product->categories()->syncWithoutDetaching([$category->id]);
+
+        return $this->showAll($product->categories);
+
     }
 
     /**
@@ -88,6 +62,6 @@ class ProductCategoryController extends ApiController
      */
     public function destroy($id)
     {
-        //
+
     }
 }
